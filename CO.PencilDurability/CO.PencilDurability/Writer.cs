2fe97b1738becfde1bf193476720a5f57ef3737d
Meredith.junk <Meredith.Junk@kumandgo.com>
2018-10-01 07:44:22 -0500

    Added Test - Return Text

---
+++ CO.PencilDurability/CO.PencilDurability/Writer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CO.PencilDurability
+{
+    public class Writer
+    {
+        public string Write()
+        {
+
+            return "Hello";
+        }
+    }
+}
--- CO.PencilDurability/PencilDurabilityTests/PencilDurabilityTest.cs
+++ CO.PencilDurability/PencilDurabilityTests/PencilDurabilityTest.cs
@@ -1,8 +1,23 @@
-﻿using System;
+﻿using CO.PencilDurability;
+using NUnit.Framework;
+using System;
 
 namespace PencilDurabilityTests
 {
-    public class Class1
+    [TestFixture]
+    public class PencilDurabilityTest
     {
+
+
+        //When the pencil is instructed to write a string of text on a sheet of paper,
+        //the paper should reflect the text that was written.
+        [TestCase]
+        public void ShouldReflectTextThatWasWritten()
+        {
+            Writer _writer = new Writer(); 
+            Assert.AreEqual("Hello", _writer.Write());
+
+        }
+
     }
 }
