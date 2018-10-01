6395b90e66b9d3dcbdccddc4468500d3b8a97620
Meredith.junk <Meredith.Junk@kumandgo.com>
2018-10-01 07:55:20 -0500

    Refactored - Test to set up attributes

--- CO.PencilDurability/PencilDurabilityTests/PencilDurabilityTest.cs
+++ CO.PencilDurability/PencilDurabilityTests/PencilDurabilityTest.cs
@@ -7,17 +7,24 @@ namespace PencilDurabilityTests
     [TestFixture]
     public class PencilDurabilityTest
     {
+        private Writer _writer;
+        private string Text = "She sells sea shells ";
 
+        [SetUp]
+        public void SetUp()
+        {
+
+            _writer = new Writer(Text); 
+        }
 
         //When the pencil is instructed to write a string of text on a sheet of paper,
         //the paper should reflect the text that was written.
         // Refactored: Text written by the pencil should always be appended to existing text on the paper. 
         //Thus, given a piece of paper with the text "She sells sea shells", when a pencil is instructed to write 
         //" down by the sea shore" on the paper, the paper will then contain the entire string (i.e. "She sells sea shells down by the sea shore").
-        [TestCase("Hello", "Hello Hello")]
+        [TestCase("down by the sea shore", "She sells sea shells down by the sea shore")]
         public void ShouldReflectTextThatWasWritten(string text, string ExpectedResult)
         {
-            Writer _writer = new Writer("Hello "); 
             Assert.AreEqual(ExpectedResult, _writer.Write(text));
 
         }
